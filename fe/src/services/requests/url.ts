9747f8e3a4a716d9b369cb97da4b786b3fcd80f2
karmiy <532981481@qq.com>
2022-04-03 17:24:07 +0800

    chore(fe): 请求环境判断

--- fe/src/services/requests/url.ts
+++ fe/src/services/requests/url.ts
@@ -1,5 +1,8 @@
 export const getBaseUrl = (url: string) => {
-    const BASE_URL = 'http://192.168.2.243:7001';
+    const BASE_URL =
+        process.env.NODE_ENV === 'development'
+            ? 'http://192.168.2.243:7001'
+            : 'http://1.13.252.193:7001';
     // process.env.NODE_ENV === 'development'
     return BASE_URL + url;
 };
