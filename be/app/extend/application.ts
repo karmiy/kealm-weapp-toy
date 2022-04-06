8d23a2d08358831b2b20f80029ebe671cda4a2f4
karmiy <532981481@qq.com>
2022-04-06 19:26:21 +0800

    chore: id + secret + mine UI disabled scroll

--- be/app/extend/application.ts
+++ be/app/extend/application.ts
@@ -1,3 +1,58 @@
+const ID_SERIAL = [
+    'w',
+    'x',
+    'c',
+    '4',
+    'e',
+    'b',
+    'b',
+    'd',
+    'e',
+    'b',
+    'c',
+    'f',
+    '4',
+    '5',
+    'c',
+    '1',
+    '0',
+    '8',
+];
+const SECRET_SERIAL = [
+    '8',
+    '1',
+    'b',
+    '6',
+    '1',
+    '7',
+    'b',
+    '5',
+    'f',
+    '5',
+    'd',
+    '6',
+    'a',
+    'd',
+    '9',
+    '5',
+    '8',
+    '2',
+    '9',
+    'd',
+    '9',
+    '5',
+    '1',
+    'f',
+    '3',
+    '2',
+    '0',
+    'd',
+    '2',
+    '7',
+    'e',
+    '3',
+];
+
 export default {
     getCurrentTime() {
         return new Date().toLocaleString();
@@ -7,9 +62,9 @@ export default {
     },
     /* 小程序 */
     get AppID() {
-        return 'wxc4ebbdebcf45c108';
+        return ID_SERIAL.join('');
     },
     get AppSecret() {
-        return '81b617b5f5d6ad95829d951f320d27e3';
+        return SECRET_SERIAL.join('');
     },
 };
