e65b07eb9b3e96f8d3ebf9a4ec587ffdcfbdee0a
karmiy <532981481@qq.com>
2022-04-15 11:37:15 +0800

    chore(fe): 生产环境请求域名

--- fe/src/services/requests/url.ts
+++ fe/src/services/requests/url.ts
@@ -2,7 +2,8 @@ export const getBaseUrl = (url: string) => {
     const BASE_URL =
         process.env.NODE_ENV === 'development'
             ? 'http://192.168.2.243:7001'
-            : 'https://81.69.219.242';
+            : 'https://www.karmiy.cn';
+    // : 'https://81.69.219.242';
     // : 'http://1.13.252.193:7001';
     // process.env.NODE_ENV === 'development'
     return BASE_URL + url;
