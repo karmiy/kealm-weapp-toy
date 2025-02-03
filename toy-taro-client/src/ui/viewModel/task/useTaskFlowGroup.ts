f6dd4ca233bc579bd888f5f413d5bd6797a5355a
Karmiy Hong <karmiy.hong@ringcentral.com>
2025-02-03 19:38:58 +0800

    feat: [client] 任务状态根据 taskType 获取 taskFlow

--- toy-taro-client/src/ui/viewModel/task/useTaskFlowGroup.ts
+++ toy-taro-client/src/ui/viewModel/task/useTaskFlowGroup.ts
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
-import { STORE_NAME } from '@core';
+import { useEffect, useMemo, useState } from 'react';
+import { isThisWeek, isToday } from 'date-fns';
+import { sdk, STORE_NAME, TASK_TYPE } from '@core';
 import { TaskFlowGroupController } from '@ui/controller';
 import { useStoreById } from '../base';
 
@@ -9,12 +10,48 @@ interface Props {
 
 export function useTaskFlowGroup(props: Props) {
   const { taskId } = props;
+  const task = useStoreById(STORE_NAME.TASK, taskId);
+  const taskType = useMemo(() => task?.type, [task?.type]);
 
   const [taskFlowIds, setTaskFlowIds] = useState<string[]>(() => {
     return TaskFlowGroupController.getInstance().getIds(taskId) ?? [];
   });
 
-  const taskFlow = useStoreById(STORE_NAME.TASK_FLOW, taskFlowIds[0]);
+  const availableTaskFlowId = useMemo(() => {
+    if (!taskType) {
+      return;
+    }
+    const storeManager = sdk.storeManager;
+
+    // 只要 taskFlow 里有一个创建时间是今天的就算
+    if (taskType === TASK_TYPE.DAILY) {
+      const flowId = taskFlowIds.find(id => {
+        const taskFlow = storeManager.getById(STORE_NAME.TASK_FLOW, id);
+        if (!taskFlow) {
+          return false;
+        }
+        return isToday(taskFlow.createTime);
+      });
+      return flowId;
+    }
+    // 只要 taskFlow 里有一个创建时间是这周的就算
+    if (taskType === TASK_TYPE.WEEKLY) {
+      const flowId = taskFlowIds.find(id => {
+        const taskFlow = storeManager.getById(STORE_NAME.TASK_FLOW, id);
+        if (!taskFlow) {
+          return false;
+        }
+        return isThisWeek(taskFlow.createTime, { weekStartsOn: 1 });
+      });
+      return flowId;
+    }
+
+    if (taskType === TASK_TYPE.TIMED || taskType === TASK_TYPE.CHALLENGE) {
+      return taskFlowIds[0];
+    }
+  }, [taskFlowIds, taskType]);
+
+  const taskFlow = useStoreById(STORE_NAME.TASK_FLOW, availableTaskFlowId);
 
   useEffect(() => {
     const controller = TaskFlowGroupController.getInstance();
