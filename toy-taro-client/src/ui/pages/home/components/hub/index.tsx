92d34fe0d7d53950fdc8c660da6fda5aed07b6e3
Karmiy Hong <karmiy.hong@ringcentral.com>
2025-02-11 22:21:18 +0800

    feat: [client] 推荐专区补充 toast 提示

--- toy-taro-client/src/ui/pages/home/components/hub/index.tsx
+++ toy-taro-client/src/ui/pages/home/components/hub/index.tsx
@@ -1,5 +1,6 @@
 import { View } from '@tarojs/components';
 import { PAGE_ID } from '@shared/utils/constants';
+import { showToast } from '@shared/utils/operateFeedback';
 import { navigateToPage } from '@shared/utils/router';
 import { WhiteSpace } from '@ui/components';
 import { Item } from './item';
@@ -22,6 +23,11 @@ const Hub = () => {
           <Item
             title='推荐专区'
             coverImage='https://gitee.com/karmiy/static/raw/master/weapp-toy/imgs/demo/demo-hub-1.png'
+            onClick={() => {
+              showToast({
+                title: '尚未开放，敬请期待',
+              });
+            }}
           />
         </View>
       </View>
